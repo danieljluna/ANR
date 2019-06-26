df75da9f6d7a61d9cb2f39899a04ec0da45a9cf1
Daniel Luna <danieljluna0@gmail.com>
2019-06-25 20:24:30 -0700

    Made a resource path relative

--- ANR/Gordian/Source/GordianEngine/ActorComponents/Private/SimpleSpriteComponent.cpp
+++ ANR/Gordian/Source/GordianEngine/ActorComponents/Private/SimpleSpriteComponent.cpp
@@ -22,7 +22,7 @@ void OSimpleSpriteComponent::Initialize(AActor* ActorInitializingFrom)
 {
 	Parent::Initialize(ActorInitializingFrom);
 
-	_FileToLoadTextureFrom = "F:/Programming/GitHub/ANR/ANR/NDB_Scraper/Output/06041.png";
+	_FileToLoadTextureFrom = "../NDB_Scraper/Output/06041.png";
 	_TextureToRender.loadFromFile(_FileToLoadTextureFrom);
 	_SpriteToRender.setTexture(_TextureToRender);
 	sf::IntRect DefaultCardRect(0, 0, 300, 419);
